28774b17b2185a5e7c4c35f52559484316d30af4
valen <valen@SHAYLA>
2025-09-28 15:02:04 +0200

    Add project files.

---
+++ GiftoftheGiversApp/Models/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace GiftoftheGiversApp.Models.Entities
+{
+    public class User    { 
+        public int id { get; set; }
+        public string Fname { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string UPass { get; set; }
+        public string Position { get; set; }
+
+
+    }
+}
---
+++ GiftoftheGiversApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GiftoftheGiversApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
